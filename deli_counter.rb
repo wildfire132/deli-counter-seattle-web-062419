868ea24b890930960bbf2832c344580815232eae
Katana Tran <tkatyk21@gmail.com>
2019-06-24 06:51:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def line(array)
+  if array[1] != ""
+    puts "The line is currently:#{index+1}. #{name}."
+  else  
+    puts "The line is currently empty."
+  end 
+end
+
+def take_a_number(array,name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{number} in line."
+end 
+
+def now_serving(array)
+  if array[1] != ""
+    puts "currently serving #{array[1]}."
+    array.shift
+  else
+    puts "There is nobody waiting to be served!"
+end
+
